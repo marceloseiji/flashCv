735dc06d5b726b8bfc1de02b74a84503626ea559
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-07-12 14:00:31 -0300

    feat: adds chakra-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 
 const container = document.getElementById('root')
 
 if (container) {
   const root = createRoot(container)
   root.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   )
 }
