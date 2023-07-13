6374562319a64a56e65e4ab2e1d926b20cb2fd4f
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-07-13 12:18:19 -0300

    chore: chakra removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
-import { Container } from '@chakra-ui/react'
 import './styles/global.scss'
 
 const container = document.getElementById('root')
@@ -10,12 +8,8 @@ const container = document.getElementById('root')
 if (container) {
   const root = createRoot(container)
   root.render(
-    <ChakraProvider>
-      <BrowserRouter>
-        <Container w="100%" h="100%" bg="blue.800">
-          <App />
-        </Container>
-      </BrowserRouter>
-    </ChakraProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   )
 }
