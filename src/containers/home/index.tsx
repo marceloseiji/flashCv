eafec225c8dd8ff224cafb1911d8fe053362d518
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-07-13 02:15:45 -0300

    style: adjust chakra

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,3 +1,36 @@
-const Home = () => <h1>HOME Testing</h1>
+import { Box, Container, Heading, Stack } from '@chakra-ui/react'
+import CvCreator from 'features/cvCreator'
+
+const Home = () => (
+  <>
+    <Stack spacing={6}>
+      <Heading as="h1" size="4xl" noOfLines={1}>
+        (4xl) In love with React & Next
+      </Heading>
+      <Heading as="h2" size="3xl" noOfLines={1}>
+        (3xl) In love with React & Next
+      </Heading>
+      <Heading as="h2" size="2xl">
+        (2xl) In love with React & Next
+      </Heading>
+      <Heading as="h2" size="xl">
+        (xl) In love with React & Next
+      </Heading>
+      <Heading as="h3" size="lg">
+        (lg) In love with React & Next
+      </Heading>
+      <Heading as="h4" size="md">
+        (md) In love with React & Next
+      </Heading>
+      <Heading as="h5" size="sm">
+        (sm) In love with React & Next
+      </Heading>
+      <Heading as="h6" size="xs">
+        (xs) In love with React & Next
+      </Heading>
+    </Stack>
+    <CvCreator />
+  </>
+)
 
 export default Home
