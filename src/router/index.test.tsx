7fba3dd63f82091c3eda591e38b702f7846e545c
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-07-17 23:45:26 -0300

    test: change home test, adds router test

--- src/router/index.test.tsx
+++ src/router/index.test.tsx
@@ -14,5 +14,5 @@ jest.mock('react-router-dom', () => {
 test('routes', () => {
   render(<Router />, { wrapper: BrowserRouter })
 
-  expect(screen.getByText(/home testing/i)).toBeInTheDocument()
+  expect(screen.getByText(/crie seu currículo online/i)).toBeInTheDocument()
 })
