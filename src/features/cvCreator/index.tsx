639ced5169ca9d756997f64e57671806ad951744
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-07-18 19:04:44 -0300

    chore: updates redux store

--- src/features/cvCreator/index.tsx
+++ src/features/cvCreator/index.tsx
@@ -1,10 +1,13 @@
 import { Typography } from '@mui/material'
+import CvStepper from './components/cvStepper'
 
 const CvCreator = () => (
   <>
     <Typography variant="h6" gutterBottom>
       CV Generator
     </Typography>
+
+    <CvStepper />
   </>
 )
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { Provider } from 'react-redux'
+import store from './store'
 import { BrowserRouter } from 'react-router-dom'
 import { CssBaseline } from '@mui/material'
 import { ThemeProvider } from '@mui/material/styles'
@@ -12,13 +14,15 @@ const container = document.getElementById('root')
 if (container) {
   const root = createRoot(container)
   root.render(
-    <BrowserRouter>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <ResponsiveMainContainer>
-          <App />
-        </ResponsiveMainContainer>
-      </ThemeProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <ResponsiveMainContainer>
+            <App />
+          </ResponsiveMainContainer>
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   )
 }
