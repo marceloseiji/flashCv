00224fb84e5495ace6d3500feeef12f88efd8ea8
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-07-19 02:53:56 -0300

    feat: home and cvCreator texts adjustments

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -3,8 +3,8 @@ import CvCreator from 'features/cvCreator'
 
 const Home = () => (
   <>
-    <Typography variant="h6" gutterBottom>
-      Crie seu currículo online.
+    <Typography variant="h6" gutterBottom sx={{ marginTop: '16px' }}>
+      Crie seu currículo online
     </Typography>
     <CvCreator />
   </>
