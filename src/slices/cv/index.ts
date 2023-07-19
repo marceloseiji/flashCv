0c813ce4276fdbe7e118348e59caed3898713b9e
Marcelo Seiji <ikuta.marcelo@gmail.com>
2023-07-19 16:10:37 -0300

    feat: slice adjust to use hasError on fild

--- src/slices/cv/index.ts
+++ src/slices/cv/index.ts
@@ -8,7 +8,14 @@ const cvSlice = createSlice({
   initialState,
   reducers: {
     setBasicInfos(state, action: PayloadAction<BasicInfos>) {
-      state.stepBasicInfos = { ...state.stepBasicInfos, ...action.payload }
+      state.stepBasicInfos = {
+        ...state.stepBasicInfos,
+        [action.payload.field]: {
+          text: action.payload[action.payload.field].text,
+          hasError: action.payload[action.payload.field].hasError
+        },
+        field: action.payload.field
+      }
     },
     setActiveStep(state, action: PayloadAction<Stepper['activeStep']>) {
       state.stepper.activeStep = action.payload
--- src/slices/cv/initialState.ts
+++ src/slices/cv/initialState.ts
@@ -1,12 +1,35 @@
-import { InitialStateProps } from './types'
+import { InitialStateProps, BasicInfosFields } from './types'
 
 const initialState: InitialStateProps = {
   stepper: {
     activeStep: 0
   },
   stepBasicInfos: {
-    name: '',
-    age: ''
+    field: BasicInfosFields.name,
+    name: {
+      text: '',
+      hasError: false
+    },
+    role: {
+      text: '',
+      hasError: false
+    },
+    age: {
+      text: '',
+      hasError: false
+    },
+    city: {
+      text: '',
+      hasError: false
+    },
+    state: {
+      text: '',
+      hasError: false
+    }
+  },
+  presentation: {
+    aboutMe: '',
+    professionalObjective: ''
   }
 }
 
